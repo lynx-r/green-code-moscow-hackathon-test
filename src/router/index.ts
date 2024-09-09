d5fc3f8ae0539c9c980537443e6aababc10795bd
Aleksey P <aleksey.p10n@gmail.com>
2024-09-09 15:06:12 +0300

    base url

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   // history: createWebHistory(import.meta.env.BASE_URL || '/green-code-moscow-hackathon-test/'),
-  history: createWebHistory('/green-code-moscow-hackathon-test/'),
+  history: createWebHistory('/'),
   routes: [
     {
       path: '/',
