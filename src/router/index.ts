6ccf4e65b69222dfd93c32e33a2d0688f362056a
Aleksey P <aleksey.p10n@gmail.com>
2024-09-09 17:46:04 +0300

    fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,8 +3,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  // history: createWebHistory(import.meta.env.BASE_URL || '/green-code-moscow-hackathon-test/'),
-  history: createWebHistory('/'),
+  history: createWebHistory(import.meta.env.BASE_URL || '/green-code-moscow-hackathon-test/'),
+  // history: createWebHistory('/'),
   routes: [
     {
       path: '/',
